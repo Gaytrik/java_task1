e32b88d709bb3efb595b52cb0b71538fc0cf2bf0
Gaytrik <gaytrikolhe2001@gmail.com>
2026-04-06 14:30:31 +0530

    complete task

--- java-oop-task-1/src/Car.java
+++ java-oop-task-1/src/Car.java
@@ -1,3 +1,26 @@
-public class Car {
-    
+class Car extends Machine implements Vehicle {
+
+    @Override
+    public void start() {
+        System.out.println("Car started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car stopped");
+    }
+
+    @Override
+    void run() {
+        System.out.println("Car is running smoothly");
+    }
+
+    public static void main(String[] args) {
+        Car c = new Car();
+
+        c.start();
+        c.run();
+        c.fuelType();
+        c.stop();
+    }
 }
--- java-oop-task-1/src/Machine.java
+++ java-oop-task-1/src/Machine.java
@@ -1,3 +1,8 @@
-public class Machine {
-    
+abstract class Machine {
+
+    void fuelType() {
+        System.out.println("Uses Petrol/Diesel");
+    }
+
+    abstract void run(); // abstract method
 }
